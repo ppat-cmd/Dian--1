f2dc207ead6f057e4e109e82f12b8ad3db615174
n <3198209810@qq.com>
2025-09-20 19:33:30 +0800

    main

--- task_1.c
+++ task_1.c
@@ -8,7 +8,7 @@ void help() {
 }
 
 
-void info(int argc, char *argv[]) {
+void command_info(int argc, char *argv[]) {
 
     if (argc < 3) {
         fprintf(stderr, "错误: 'info' 命令需要提供一个名字参数\n");
@@ -35,14 +35,14 @@ int main(int argc, char *argv[]) {
             fprintf(stderr, "'info' 命令不需要额外参数，多余的参数将被忽略\n");
         }
         else{
-        command_info();
+        command_info(argc,argv);
         }
     } 
 
     else if (strcmp(argv[1], "--help") == 0) {
         help();
     }  
-    
+
     else {
         fprintf(stderr, "错误: 未知命令 '%s'\n", argv[1]);
         fprintf(stderr, "请使用 '--help' 查看支持的命令\n");
